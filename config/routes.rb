3a3cc54c3cef9452d48360e8ae2c83765ac1c9df
daoduydat <taddd1993@gmail.com>
2016-04-12 13:12:33 +0700

    task_59923_implement_fullcalendar_api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "static_pages#index"
   devise_for :users
+  resources :users, only: :show
+  resources :calendars
   resources :users, only: :show do
     resources :calendars
   end

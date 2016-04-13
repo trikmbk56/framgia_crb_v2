ab24d47180141977f58a475a7b6d0c5c8c99b655
Phorn Sopheak <phornsopheak17@gmail.com>
2016-04-13 11:14:12 +0700

    clear all event

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   resources :users, only: :show
   resources :calendars
   resources :users, only: :show do
-    resources :calendars
+    resources :calendars do
+      resource :destroy_events, only: :destroy
+    end
     resources :events, only: [:edit, :show]
   end
   resources :calendars, only: [:index, :new, :create]

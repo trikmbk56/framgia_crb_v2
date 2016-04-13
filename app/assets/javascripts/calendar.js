758588036687c04b622854e38d6a4d4f0146148e
nguyenthaison <nguyenthaison9193@gmail.com>
2016-04-13 11:27:49 +0700

    show event when click in minicalendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -5,8 +5,25 @@ $(document).ready(function() {
       center: 'title',
       right: 'month,agendaWeek,agendaDay'
     },
-    defaultDate: '2016-01-12',
+    defaultView: 'agendaWeek',
     businessHours: true,
     editable: true
+  });
+
+  $('#miniCalendar').datepicker({
+    dateFormat: 'DD, d MM, yy',
+      onSelect: function(dateText,dp){
+        $('#calendar').fullCalendar('gotoDate',new Date(Date.parse(dateText)));
+        $('#calendar').fullCalendar('changeView','agendaDay');
+      }
+  });
+
+  $('.create').click(function(){
+    if ($(this).parent().hasClass('open')) {
+      $(this).parent().removeClass('open');
+    }
+    else{
+      $(this).parent().addClass('open');
+    };
   });
 });

714588f22ebad82d1dfae761aea215fd7e8a6c53
daoduydat <taddd1993@gmail.com>
2016-04-14 11:12:17 +0700

    task_59764_drag_drop_create_event

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def title page_title
     content_for :title, page_title.to_s
   end
+
+  def flash_class level
+    case level
+      when :notice then "alert-info"
+      when :error then "alert-error"
+      when :alert then "alert-warning"
+      when :success then "alert-success"
+    end
+  end
 end

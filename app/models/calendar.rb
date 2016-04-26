a02faf91dd0e38ed185d0a390b7b50c448ff3636
ngothaiminh <ngothaiminh1123@gmail.com>
2016-04-26 20:32:47 +0700

    add sub calendar

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -2,9 +2,10 @@ class Calendar < ActiveRecord::Base
   has_many :events, dependent: :destroy
   has_many :user_calendars, dependent: :destroy
   has_many :users, through: :user_calendars
+  has_many :sub_calendars, class_name: Calendar.name, foreign_key: :parent_id
 
   belongs_to :color
   belongs_to :owner, class_name: User.name
 
-  ATTRIBUTES_PARAMS = [:name, :description, :user_id, :color_id]
+  ATTRIBUTES_PARAMS = [:name, :description, :user_id, :color_id, :parent_id]
 end
